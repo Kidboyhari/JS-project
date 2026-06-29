1d3df1bf64703154728e69ac54505fb6b119a6ac
HariKrishna <kidboyhari@gmail.com>
2026-06-29 13:54:52 +0530

    make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -160,14 +160,20 @@ product.forEach((products) => {
                     matchingItem = item;
                 }
             });
+        
             if (matchingItem) {
                 matchingItem.quantity += 1;
             }else{
                 cart.push({
                 productName: productName,
                 quantity: 1 }
-        )};
-            console.log(cart);
+        )}
+            let cartQuantity = 0;
+            cart.forEach((item) => {
+                cartQuantity += item.quantity;
+            });
+            document.querySelector('.js-cart-quantity')
+            .innerHTML = cartQuantity;
         });
     });
 });
